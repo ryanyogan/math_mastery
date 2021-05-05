eafd40f4967c7f76b6f211509b6499e0f1869cbe
ryan <ryanyogan@gmail.com>
2021-05-05 10:28:52 -0500

    QuizManager is a singleton process started by the application now

--- lib/mastery/application.ex
+++ lib/mastery/application.ex
@@ -2,10 +2,13 @@ defmodule Mastery.Application do
   @moduledoc false
 
   use Application
+  alias Mastery.Boundary.QuizManager
 
   @impl true
   def start(_type, _args) do
-    children = []
+    children = [
+      {QuizManager, [name: QuizManager]}
+    ]
 
     opts = [strategy: :one_for_one, name: Mastery.Supervisor]
     Supervisor.start_link(children, opts)
